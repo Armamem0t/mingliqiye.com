94817b4daf8219f041afbce7dc7217f6a8381959
MingLi <Armament@email.cn>
2023-01-25 20:41:26 +0800

    github

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "我的第一个 JavaScript 函数";
 }
+
+function Appendzero(obj) {
+    if (obj < 10) return "0" + "" + obj;
+    else return obj;
+}
+
+function changeClock() {
+    var d = new Date();
+    document.getElementById("date_c").innerHTML = Appendzero(d.getFullYear()) + "-" + Appendzero((d.getMonth() + 1)) + "-" + Appendzero(d.getDate());
+    document.getElementById("clock_c").innerHTML = Appendzero(d.getHours()) + ":" + Appendzero(d.getMinutes()) + ":" + Appendzero(d.getSeconds());
+}
+window.setInterval(changeClock, 1000);
